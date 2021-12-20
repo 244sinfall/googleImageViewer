7517e99b10e0b2fe216a261fd122c92dd294d74c
Dmitry Filin <dimafilin6@icloud.com>
2021-12-20 15:34:06 +0700

    Initial Commit

---
+++ Google Image Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google Image Viewer
+//
+//  Created by Дмитрий Филин on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
